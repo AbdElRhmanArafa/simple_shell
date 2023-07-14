a0f150c8ae06b6d28d0732276c0251b6b0eb8a11
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-07-14 06:53:14 +0300

    Add Shell Utils file

---
+++ shell_utils.c
@@ -0,0 +1,52 @@
+#include "simple_shell.h"
+/**
+ * prompt - use to display in interactive mode
+ *
+ * Return - void
+ */
+void prompt(void)
+{
+	_puts("$ ");
+	fflush(stdout);
+}
+/**
+ * parse_input - split input
+ *
+ * @input: input will be pases
+ * Return: token Array of string
+ */
+char **parse_input(char *input)
+{
+	int bufsize = 64;
+	int position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	token = _strtok(input, " \t\n");
+	while (token != NULL)
+	{
+		tokens[position] = _strdup(token);
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				perror("realloc");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = _strtok(NULL, " \t\n");
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
